cfee805d1cf0f4ad1f70c6d3a6836252d99e011e
wcy <1101697681@qq.com>
2016-10-20 18:19:43 +0800

    create

---
+++ src/main/java/com/randioo/config/randioo_excel/RandiooConfigExcelApp.java
@@ -0,0 +1,49 @@
+package com.randioo.config.randioo_excel;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FilenameFilter;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.dom4j.Document;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
+
+/**
+ * Hello world!
+ *
+ */
+public class RandiooConfigExcelApp {
+	public static void main(String[] args) throws Exception {
+		System.out.println("Hello World!");
+
+		SAXReader sax = new SAXReader();
+		// Step 1 目录
+		Document doc = sax.read("./xml/config.xml");
+		Element root = doc.getRootElement();
+
+		Iterator<Element> nodeIt = root.elementIterator();
+		while (nodeIt.hasNext()) {
+			Element nodeE = nodeIt.next();
+			String xlsxName = nodeE.attributeValue("xlsx");
+			String page = nodeE.attributeValue("page");
+			Sheet sheet = getSheet("excelFile", xlsxName, page);
+			System.out.println(sheet.getSheetName());
+		}
+
+	}
+
+	public static Sheet getSheet(String path, String xlsxName, String sheetName) throws Exception {
+		File f = new File(new StringBuilder(String.valueOf(System.getProperty("user.dir")))
+				.append(System.getProperty("file.separator")).append(path).append(System.getProperty("file.separator"))
+				.append(xlsxName).toString());
+		FileInputStream fis = new FileInputStream(f);
+		Workbook workbook = new XSSFWorkbook(fis);
+		return workbook.getSheet(sheetName);
+	}
+}
