eaa66e24875c3e8fc454d494f88e154a449a91a7
wcy <1101697681@qq.com>
2017-01-12 15:10:30 +0800

    修复获取sheet时数据源没有关闭的问题

--- src/main/java/com/randioo/config/randioo_excel/util/ExcelUtils.java
+++ src/main/java/com/randioo/config/randioo_excel/util/ExcelUtils.java
@@ -2,6 +2,7 @@ package com.randioo.config.randioo_excel.util;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
@@ -17,13 +18,33 @@ public class ExcelUtils {
 	 * @author wcy 2016年12月16日
 	 */
 	public static Sheet sheet(File f, String page) {
+		FileInputStream fis = null;
+		Workbook workbook = null;
 		try {
-			FileInputStream fis = new FileInputStream(f);
-			@SuppressWarnings("resource")
-			Workbook workbook = new XSSFWorkbook(fis);
-			return workbook.getSheet(page);
+			fis = new FileInputStream(f);
+			workbook = new XSSFWorkbook(fis);
+			Sheet sheet = workbook.getSheet(page);
+			
+			return sheet;
 		} catch (Exception e) {
 			e.printStackTrace();
+		}finally{
+			if(fis !=null){
+				try {
+					fis.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}							
+			}
+			if(workbook!=null){
+				try {
+					workbook.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}				
+			}
 		}
 		return null;
 	}
